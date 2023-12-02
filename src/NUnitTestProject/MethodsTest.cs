a1f8b13ad78082e25a7155d2fcca707a913750d3
cilerrsahin06@hotmail.com <cilerrsahin06@hotmail.com>
2023-12-02 22:12:41 +0300

    Custom methods..

---
+++ src/NUnitTestProject/MethodsTest.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+
+namespace NUnitTestProject
+{
+    public class MethodsTest
+    {
+
+        /// <summary>
+        /// Enter text
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="element"></param>
+        /// <param name="value"></param>
+        /// <param name="elementType"></param>
+        public static void EnterText(IWebDriver driver, string element, string value, string elementType)
+        {
+            if (elementType == "id")
+            {
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            }
+            else if (elementType == "Name")
+            {
+                driver.FindElement(By.Name(element)).SendKeys(value);
+            }
+        }
+
+
+        public static void Click(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "id")
+            {
+                driver.FindElement(By.Id(element)).Click();
+            }
+            else if (elementType == "Name")
+            {
+                driver.FindElement(By.Name(element)).Click();
+            }
+        }
+    }
+}
--- src/NUnitTestProject/UnitTest.cs
+++ src/NUnitTestProject/UnitTest.cs
@@ -13,8 +13,8 @@ namespace NUnitTestProject
         [SetUp]
         public void Initiliaze()
         {
-            driver.Navigate().GoToUrl("http://www.google.com.tr");
-            Console.WriteLine("Google sayfasý açýldý.");
+            driver.Navigate().GoToUrl("https://giris.hepsiburada.com/?ReturnUrl=https%3A%2F%2Foauth.hepsiburada.com%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3DSPA%26redirect_uri%3Dhttps%253A%252F%252Fwww.hepsiburada.com%252Fuyelik%252Fcallback%26response_type%3Dcode%26scope%3Dopenid%2520profile%26state%3D1931deb80cf44aa99607cfae43df5c9a%26code_challenge%3DUe4sEF6HJzGEAN6i9ydBBCehmOneAzB_bNVW7kAcHQY%26code_challenge_method%3DS256%26response_mode%3Dquery%26ActivePage%3DPURE_LOGIN%26oidcReturnUrl%3Dhttps%253A%252F%252Fwww.hepsiburada.com%252F");
+            Console.WriteLine("Hepsiburada sayfasý açýldý.");
         }
 
         /// <summary>
@@ -23,10 +23,16 @@ namespace NUnitTestProject
         [Test]
         public void ExecuteTest()
         {
-            string yazilanYazi = "Çiler Sürücü Kursu";
-            IWebElement webElement = driver.FindElement(By.Name("q"));
-            webElement.SendKeys(yazilanYazi);
-            Console.WriteLine("Arama kýsmýna " + yazilanYazi + " yazildi.");
+            //IWebElement element = driver.FindElement(By.Name("q"));
+
+            MethodsTest.EnterText(driver, "username", "burakc34@gmail.com", "Name");
+
+            System.Threading.Thread.Sleep(10000);
+
+            //string yazilanYazi = "Çiler Sürücü Kursu";
+            //IWebElement webElement = driver.FindElement(By.Name("q"));
+            //webElement.SendKeys(yazilanYazi);
+            //Console.WriteLine("Arama kýsmýna " + yazilanYazi + " yazildi.");
         }
 
         /// <summary>
